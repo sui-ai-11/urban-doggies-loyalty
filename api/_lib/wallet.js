04c060a8fd76701448ca459293301edf68fcfb55
sui-ai-11 <maumarasigan888@gmail.com>
2026-02-26 20:00:04 +0800

    Update wallet.js

--- api/_lib/wallet.js
+++ api/_lib/wallet.js
@@ -43,7 +43,7 @@ async function updateWalletPass(clientToken, stamps, totalVisits, cardsCompleted
       return;
     }
 
-    var objectId = ISSUER_ID + '.' + clientToken + '_v3';
+    var objectId = ISSUER_ID + '.' + clientToken + '_v4';
     var apiBase = 'https://walletobjects.googleapis.com/walletobjects/v1/loyaltyObject/';
 
     // First try to GET the object to see if it exists
